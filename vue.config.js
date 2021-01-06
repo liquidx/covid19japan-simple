02a325d9752333ea5c2262f61175801ef611ed38
Alastair Tse <alastair@liquidx.net>
2021-01-07 00:11:03 +0900

    Custom port

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   publicPath: '/covid19japan-simple/',
-  outputDir: '../covid19japan-simple-site/docs'
+  outputDir: '../covid19japan-simple-site/docs',
+  devServer: {
+    host: '127.0.0.1',
+    port: 8088,
+    public: 'localhost:8088/covid19japan-simple/'
+  },
 };
