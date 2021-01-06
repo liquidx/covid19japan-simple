3dcdd627e6cbaea1ef18b54bf3b6640df4bc48d2
Alastair Tse <alastair@liquidx.net>
2021-01-06 23:51:06 +0900

    Add description

---
+++ src/components/Description.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="description">
+    <p>
+      All you need to know is whether the number of cases are <b>increasing or decreasing compared to last week.</b>
+      This page tells you either on a weekly or monthly basis whether the number of new cases,
+      active cases, deaths or critical cases are going up or down.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.description {
+  margin-top: 5em;
+  padding: 0 1em 0 0;
+}
+</style>
